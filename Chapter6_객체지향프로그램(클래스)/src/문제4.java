5b10e92d306cf09fdda031e6387a1548f36c7206
user <user@DESKTOP-COJ9858>
2022-11-15 09:57:27 +0900

    Chapter6

---
+++ Chapter6_객체지향프로그램(클래스)/src/문제4.java
@@ -0,0 +1,23 @@
+class Member{
+	String id;
+	String password;
+	String checkPassword;
+	String name;
+	String nickname;
+	int num;
+	int birth;
+	String sex;
+	String email;
+	String address;
+	int number;
+	int phone;
+	
+}
+public class 문제4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
